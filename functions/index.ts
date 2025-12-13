d971aa7d39df2d3919402bab2d47e98769517013
Matthew Reardon <patriotnewsactivism@proton.me>
2025-12-13 15:33:40 -0700

    updates

--- functions/index.ts
+++ functions/index.ts
@@ -10,7 +10,7 @@
  * - Scream Leaderboard tracking
  */
 
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions/v1';
 import * as admin from 'firebase-admin';
 import Stripe from 'stripe';
 
