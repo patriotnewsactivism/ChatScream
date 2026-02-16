55a4dfdd6a5e470ec009550eed141a52ac4b993f
Matthew Reardon <patriotnewsactivism@proton.me>
2026-02-16 08:12:43 -0600

    a

--- api/[...all].js
+++
@@ -1,8 +0,0 @@
-import app from '../server/app.js';
-
-export default (req, res) => {
-  if (typeof req.url === 'string' && !req.url.startsWith('/api')) {
-    req.url = req.url.startsWith('/') ? `/api${req.url}` : `/api/${req.url}`;
-  }
-  return app(req, res);
-};
